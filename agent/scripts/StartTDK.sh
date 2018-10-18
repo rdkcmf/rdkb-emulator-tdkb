8728be413989cc4b6f01aa9021e1e74187f9ce5d
Stephen Barrett <stephen.barrett@accenture.com>
2018-10-18 17:14:47 +0100

    Import of Comcast source (stable2)
    Change-Id: If24aaea0e29d64f7a7d4f05e441eb77df140d7c1
    Source: https://gerrit.teamccp.com/rdk/rdkb/components/generic/tdkb/devices/intel-x86-pc/rdk-broadband
    SHA1: 920d73c63b4a769f6c948fff31fa0cf727564b2f

---
+++ agent/scripts/StartTDK.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+##########################################################################
+# If not stated otherwise in this file or this component's Licenses.txt
+# file the following copyright and licenses apply:
+#
+# Copyright 2016 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##########################################################################
+#
+
+#Setting up environment to run TDK
+export TDK_PATH=/opt/TDK/
+export PATH=$PATH:/usr/local/bin:$TDK_PATH
+export TDK_LIB_PATH=$TDK_PATH/libs/
+export LD_LIBRARY_PATH=$TDK_PATH/libs/:/usr/local/lib/:/usr/local/Qt/lib/:/mnt/nfs/lib:/mnt/nfs/bin/target-snmp/lib/:/mnt/nfs/bin:/usr/local/lib/sa:$LD_LIBRARY_PATH
+
+export PATH HOME LD_LIBRARY_PATH
+ulimit -c unlimited
+echo "Going to start Agent"
+cd $TDK_PATH/
+sh TDKagentMonitor.sh &
+./rdk_tdk_agent_process
