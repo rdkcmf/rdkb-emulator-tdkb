f3cf57725892020ce680bcc2d74df7e204e709ff
svelus003c <sugasini_velusamy@comcast.com>
2020-09-22 02:58:08 +0000

    TDK-3060 : Modified EMU TDK platform specific scripts
    Reason for change: Made changes in the platform specific
    scripts for EMU to handle the changes done to support
    RDM.
    Test Procedure: Check whether TDK is up and running during
    bootup
    Risks: None
    Signed-off-by: svelus003c <sugasini_velusamy@comcast.com>
    Change-Id: Ibfd98861821988e9a75c0dd028e5c3b9f403d520

--- agent/scripts/StopTDK.sh
+++ agent/scripts/StopTDK.sh
@@ -29,7 +29,7 @@ source $TDK_PATH/Rdklogger_post-requisite.sh
 #Killing inactive TDK processes
 ps -ef | grep "TDKagentMonitor" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
 sleep 1
-ps -ef | grep "tdk_agent" | grep -v "grep" | grep -v "tr69agent" | awk '{print $2}' | xargs kill -9 >& /dev/null
+ps -ef | grep "tdk_agent" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
 ps -ef | grep "tftp" | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
 ps -ef | grep $TDK_PATH | grep -v "grep" | awk '{print $2}' | xargs kill -9 >& /dev/null
 sleep 2
